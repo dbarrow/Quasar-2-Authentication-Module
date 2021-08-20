9308dc4ba711591bb8f9a526c18e66b7e054995e
Darryl Barrow <darryl@traversepoint.com>
2021-08-20 05:31:21 -0400

    Delete auth.config.js

--- auth.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-    USER_LOCAL_STORAGE_KEY : 'tpuser',
-   // BASE_API_URL : 'http://api.test/api/auth/',
-    BASE_API_URL : 'http://147.182.210.32/api/auth/',
-
-    SUCCESSFUL_LOGIN_ROUTE : '/users',
-}
