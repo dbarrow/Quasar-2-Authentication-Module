26e9054489c7a68135b95abec3459edfb9ede41c
Darryl Barrow <darryl@travesepoint.com>
2021-08-20 05:36:59 -0400

    cleanup

--- auth.config.example.js
+++ auth.config.example.js
@@ -1,5 +1,5 @@
 export default {
-    USER_LOCAL_STORAGE_KEY : 'tpuser',
+    USER_LOCAL_STORAGE_KEY : 'user',
     BASE_API_URL : 'http://api.test/api/auth/',
-    SUCCESSFUL_LOGIN_ROUTE : '/users',
+    SUCCESSFUL_LOGIN_ROUTE : '/app',
 }
