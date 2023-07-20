3c0afc9e94dfa8e0965787c37f3ca9d028c865bc
Darryl Barrow <darryl@traversepoint.com>
2023-07-20 14:04:49 -0400

    added example auth config file

---
+++ auth.config.example.js
@@ -0,0 +1,7 @@
+export default {
+    USER_LOCAL_STORAGE_KEY : 'user',
+    BASE_API_URL : 'http://localhost/api/auth/',
+    USER_API_URL : 'http://localhost/api/users/',
+
+    SUCCESSFUL_LOGIN_ROUTE : '/app',
+}
